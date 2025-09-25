3acafbd41483c93ed72f16f4089dfe6f647072e6
Severin Bergsmann <severin.bergsmann@gmail.com>
2025-09-25 09:36:02 +0000

    I already did that

--- src/firebase/auth/actions.ts
+++ src/firebase/auth/actions.ts
@@ -5,13 +5,23 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
   signOut as firebaseSignOut,
+  connectAuthEmulator,
 } from "firebase/auth";
 import { auth } from "@/firebase/client";
+import { firebaseConfig } from "@/firebase/config";
 
 const provider = new GoogleAuthProvider();
 
 export const signInWithGoogle = async () => {
   try {
+    // Sometimes in dev environments, explicitly setting the auth domain can help.
+    auth.tenantId = null;
+    auth.languageCode = 'en';
+    if (firebaseConfig.authDomain) {
+      auth.useDeviceLanguage();
+      auth.settings.authDomain = firebaseConfig.authDomain;
+    }
+
     await signInWithPopup(auth, provider);
   } catch (error) {
     console.error("Error signing in with Google: ", error);
