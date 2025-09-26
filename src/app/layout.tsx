a31556f3df2ef8a66529105a3b5fe24054291853
Severin Bergsmann <severin.bergsmann@gmail.com>
2025-09-26 10:46:57 +0000

    Apply this instructions correctly: Install our package
    Start by install

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 import type { Metadata } from "next";
 import { cn } from "@/lib/utils";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -26,6 +27,7 @@ export default function RootLayout({
       <body className={cn("font-body antialiased h-full")}>
         {children}
         <SpeedInsights/>
+        <Analytics />
       </body>
     </html>
   );
