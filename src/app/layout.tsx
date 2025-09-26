002159e12281793e23c731be82f3f8044da36e5d
Severin Bergsmann <severin.bergsmann@gmail.com>
2025-09-26 09:19:47 +0000

    Try fixing this error: `Console Error: In HTML, <html> cannot be a child

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 
 import type { Metadata } from "next";
+import { cn } from "@/lib/utils";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Nutrack9",
@@ -13,8 +16,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className="h-full">
+       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Sora:wght@400;600;700&display=swap" rel="stylesheet" />
+        <meta name="theme-color" content="#F87171" />
+      </head>
+      <body className={cn("font-body antialiased h-full")}>
+        {children}
+        <SpeedInsights/>
+      </body>
     </html>
   );
 }
